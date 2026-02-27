0a22853f6618c46a2584a1f57c317e5f2cdce99c
Tedd Kim <teddkimnskills@gmail.com>
2026-02-27 12:33:53 +0300

    chore: initialise API package

---
+++ src/test/java/com/teddkim/taskmanagerAPI/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teddkim.taskmanagerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
