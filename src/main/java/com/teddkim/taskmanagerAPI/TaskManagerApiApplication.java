f9258bc6092e48780bfafd986ff6d84d111a8ab2
Tedd Kim <teddkimnskills@gmail.com>
2026-03-04 22:31:55 +0300

    feat: add simple Create logic

--- src/main/java/com/teddkim/taskmanagerAPI/TaskManagerApiApplication.java
+++ src/main/java/com/teddkim/taskmanagerAPI/TaskManagerApiApplication.java
@@ -3,11 +3,47 @@ package com.teddkim.taskmanagerAPI;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class TaskManagerApiApplication {
+
+  class Task {
+    //properties! our class have on start
+    private int id;
+    private String tittle;
+    private boolean done;
+
+    // constructor, where objects/instances will be born or made from
+    Task(String Tittle) {
+      this.tittle = Tittle;
+      this.id = 0;
+      this.done = false;
+    }
+
+    int getID() { return id; }
+    String getTittle() { return tittle; }
+    boolean isDone() { return done; }
+
+    void toggleDone() { this.done = !this.done; } // just fancy equate to true
+  }
+
+
+
+
+
+
+
+
+
 
   public static void main(String[] args) {
     SpringApplication.run(TaskManagerApiApplication.class, args);
   }
 
+  Scanner scanner = new Scanner(System.in);
+
+  Task tasks = new Task(Tittle);
+  System.out.println("Enter new task Titlle: ");
+  Tittle = scanner.nextLine();
 }
